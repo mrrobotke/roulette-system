7fe7273873f69ff373cabf153a189defac4ac43b
Nicole <nkathanicolegmail.com>
2024-11-06 23:39:39 +0300

    Added tables frontend functionality

--- tracker/models.py
+++ tracker/models.py
@@ -1,41 +1,68 @@
 from django.db import models
 from .enums import Color, Parity, Range
-    
-    
+
 # Create your models here.
+COLOR_CHOICES = [(color.value, color.name) for color in Color]
+PARITY_CHOICES = [(par.value, par.name) for par in Parity]
+RANGE_CHOICES = [(ranges.value, ranges.name) for ranges in Range]
+
+
+class User(models.Model):
+    
+    # Stores user's credentials
+    email = models.EmailField()
+    password = models.CharField(max_length=100)
 
 class BlackRedEntry(models.Model):
-    
-    COLOR_CHOICES = [(color.value, color.name) for color in Color]
-    
+
     color = models.CharField(
         max_length=2,
         choices=COLOR_CHOICES,
         default=Color.OTHER.value
     )
-     
+
     timestamp = models.DateTimeField(auto_now_add=True)
     marked = models.BooleanField(default=False)  # For 'X' marker
-    
+
 class OddEvenEntry(models.Model):
-    PARITY_CHOICES = [(par.value, par.name) for par in Parity]
-    
-    parity = models.IntegerField(
+
+    parity = models.CharField(
+        max_length=2,
         choices=PARITY_CHOICES,
         default=Parity.ERR.value
     )
     timestamp = models.DateTimeField(auto_now_add=True)
-    marked = models.BooleanField(default=False)  # For 'X' marker  
-    
+    marked = models.BooleanField(default=False)  # For 'X' marker
 
 class HighLowEntry(models.Model):
-    
-    RANGE_CHOICES = [(ranges.value, ranges.name) for ranges in Parity]
-    
-    range_value = models.IntegerField(
+
+    range_value = models.CharField(
+        max_length=2,
         choices=RANGE_CHOICES,
         default=Range.ERR.value
     )
-    
+
+    marked = models.BooleanField(default=False)
+
+class NumberStatistics(models.Model):
+
+    # Store all records
     timestamp = models.DateTimeField(auto_now_add=True)
-    marked = models.BooleanField(default=False) 
+    color = models.CharField(
+        max_length=2,
+        choices=COLOR_CHOICES,
+        default=Color.OTHER.value
+    )
+
+    parity = models.CharField(
+        max_length=2,
+        choices=PARITY_CHOICES,
+        default=Parity.ERR.value
+    )
+
+    range_value = models.CharField(
+        max_length=2,
+        choices=RANGE_CHOICES,
+        default=Range.ERR.value
+    )
+
